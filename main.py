f279f4d8d4778366c69cbbbd9e1bb7bd616a71ab
alex-ac <alexandre.abouchahine@mistral.ai>
2025-04-08 00:14:39 +0200

    add style

--- main.py
+++ main.py
@@ -1,53 +1,123 @@
 from textual import work, on
-from textual.app import App
+from textual.app import App, RenderResult
 
-
-from textual.widgets import Header, Footer, Input, Markdown
-
+from textual.widgets import Header, Footer, Input, Markdown, Static
 from mistral import start_conversation, append_conversation
-from textual.containers import VerticalScroll
+from textual.containers import VerticalScroll, Container
+from rich_pixels import Pixels, HalfcellRenderer
+from PIL import Image
 
-class Message(Markdown):
+class UserMessage(Markdown):
     ...
+
+class AssistantMessage(Markdown):
+    ...
+
+class Prompt(Input):
+    ...
+
+class Logo(Static):
+
+    def render(self) -> RenderResult:
+        pixels = Pixels.from_image_path(
+            "./resources/logo.png",
+            resize=(45, 37),
+            renderer=HalfcellRenderer()
+        )
+        return pixels
+
+class Img(Static):
+
+    def __init__(self, image: Image, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.image = image
+
+    def render(self) -> RenderResult:
+        pixels = Pixels.from_image(
+            self.image,
+            resize=(45, 37),
+            renderer=HalfcellRenderer()
+        )
+        return pixels
 
 
 class LeChat(App):
     conversation_id: str | None = None
 
-    _messages: list[str]
+    AUTO_FOCUS = "Input"
+
+    CSS = """
+    UserMessage {
+        background: black 0%;
+        border: round #242427;
+        color: $text;
+        margin: 1;
+        margin-right: 8;
+        padding: 1 2 0 1;
+    }
+
+    AssistantMessage {
+        background: black 0%;
+        color: $text;         
+        margin: 1;
+        border: round orange 50%;
+        margin-left: 8;
+        padding: 1 2 0 1;
+    }
+    
+    Prompt {
+        border: round orange 50%;
+    }
+    
+    #logo-container {
+        height: 50%;
+        width: 100%;
+        position: relative;
+        align: center middle;
+        text-align: center;
+    }
+    """
 
     def compose(self):
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
         with VerticalScroll(id="messages"):
-            yield Message()
-        yield Input(placeholder="How can I help you?")
+            with Container(id="logo-container"):
+                yield Logo(id="logo")
+        yield Prompt(placeholder="Ask le chat")
 
     @on(Input.Submitted)
     async def on_input(self, event: Input.Submitted) -> None:
         messages = self.query_one("#messages")
         event.input.clear()
-        await messages.mount(Message(event.value))
-        await messages.mount(message := Message())
-        message.anchor()
-        self.send_prompt(event.value, message)
+        await messages.remove_children("#logo")
+        await messages.mount(UserMessage(event.value))
+        self.receive_result(event.value)
 
-    @work(thread=True)
-    def send_prompt(self, prompt: str, message: Message) -> None:
+    @work()
+    async def receive_result(
+        self,
+        prompt: str
+    ) -> None:
+        messages = self.query_one("#messages")
+        await messages.mount(message := AssistantMessage())
+        message.anchor()
         content = ""
         if self.conversation_id is None:
             resp = start_conversation(inputs=prompt)
-            for chunk in resp:
-                self.conversation_id = chunk["conversation_id"]
-                content += chunk["outputs"]
-                self.call_from_thread(message.update, content)
         else:
             resp = append_conversation(conversation_id=self.conversation_id, inputs=prompt)
-            for chunk in resp:
+        for chunk in resp:
+            self.conversation_id = chunk["conversation_id"]
+            if chunk["image"]:
+                await messages.mount(Img(image_id=chunk["image"]))
+                await messages.mount(message := AssistantMessage())
+                message.anchor()
+                content = ""
+            if chunk["outputs"]:
                 content += chunk["outputs"]
-                self.call_from_thread(message.update, content)
-
+                await message.update(content)
 
 if __name__ == "__main__":
     app = LeChat()
--- mistral.py
+++ mistral.py
@@ -1,8 +1,10 @@
+import io
 import os
-from typing import TypedDict, AsyncGenerator, Generator
+from typing import TypedDict, AsyncGenerator, Generator, NotRequired
 
 import httpx
-from mistralai_private import MistralPrivate, TextChunk
+from PIL import Image
+from mistralai_private import MistralPrivate, ToolReferenceChunk, ToolExecutionStartedEvent, ToolFileChunk
 from mistralai_private.models.conversationevents import ResponseStartedEvent, MessageOutputEvent, ConversationEvents
 
 url = "https://api.mistral.ai/"
@@ -26,6 +28,7 @@ DEFAULT_MODEL = "mistral-large-2411"
 class ConversationStartResponse(TypedDict):
     conversation_id: str
     outputs: str
+    image: Image
 
 
 def parse_conversation_event(
@@ -33,32 +36,51 @@ def parse_conversation_event(
     conversation_id: str = "",
 ) -> ConversationStartResponse:
     outputs = ""
-    print(f"{event=}")
+    image = None
+    ref_count = 0
     if isinstance(event.data, ResponseStartedEvent):
         conversation_id = event.data.conversation_id
     if isinstance(event.data, MessageOutputEvent):
         if isinstance(event.data.content, str):
             outputs = event.data.content
-        elif isinstance(event.data.content, list):
-            for content in [
-                c for c in event.data.content
-                if isinstance(c, TextChunk)
-            ]:
-                outputs += content.text
-
-    print(f"{outputs=}")
+        elif isinstance(event.data.content, ToolReferenceChunk):
+            outputs += f" [[{event.data.content.title}]({event.data.content.url})] "
+            ref_count += 1
+        elif isinstance(event.data.content, ToolReferenceChunk):
+            outputs += f" [[{event.data.content.title}]({event.data.content.url})] "
+            ref_count += 1
+        elif isinstance(event.data.content, ToolFileChunk):
+            file_bytes = client.files.download(file_id=event.data.content.file_id).read()
+            image = Image.open(io.BytesIO(file_bytes))
+    if isinstance(event.data, ToolExecutionStartedEvent):
+        if outputs:
+            outputs += "\n"
+        outputs += "Searching the web\n\n"
     return ConversationStartResponse(
         conversation_id=conversation_id,
-        outputs=outputs
+        outputs=outputs,
+        image=image
     )
 
 
 def start_conversation(
     inputs: "str"
 ) -> Generator[ConversationStartResponse, None, None]:
+    name_resp = client.chat.complete(
+        model=DEFAULT_MODEL,
+        messages=[
+            {
+                "role": "user",
+                "content": f"Create a title for the conversation with the first message being: \n\n{inputs}"
+            }
+        ],
+        temperature=0,
+    )
     resp = client.beta.conversations.start_stream(
         model=DEFAULT_MODEL,
         inputs=inputs,
+        name=name_resp.choices[0].message.content,
+        tools=[{"type": "web_search"}]
     )
     conversation_id = ""
     for event in resp:
